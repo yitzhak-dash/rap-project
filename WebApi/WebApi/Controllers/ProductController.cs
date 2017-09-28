ca4aab96eadea28d12bf16b046878517dba89249
yitzhak <idashevsky@gmail.com>
2017-09-27 22:57:59 -0700

    server

--- WebApi/WebApi/Controllers/ProductController.cs
+++ WebApi/WebApi/Controllers/ProductController.cs
@@ -6,12 +6,14 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using WebApi.DataAccess;
 using WebApi.Filters;
 using WebApi.Models;
 
 namespace WebApi.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     [RoutePrefix("api/product")]
     public class ProductController : ApiController
     {
---
+++ WebApi/WebApi/Validators/EnumNameValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Validators
+{
+    public class EnumNameValidator : ValidationAttribute
+    {
+        private readonly Type _enumType;
+
+        public EnumNameValidator(Type enumType)
+        {
+            _enumType = enumType;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (Enum.GetNames(_enumType).Contains(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(this.FormatErrorMessage(validationContext.MemberName));
+        }
+    }
+}
